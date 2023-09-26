0f0f46a2f4b8388033ec5d66a16dcb96a82d5d9a
IlyaTsuba <ilyatsubablr@gmail.com>
2023-09-26 11:36:02 +0300

    fixed account activation

--- shop/catalog/models.py
+++ shop/catalog/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Discount(models.Model):
+    percent = models.IntegerField()
+    name = models.CharField(max_length=100)
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class Promocode(models.Model):
+    name = models.CharField(max_length=10)
+    percent = models.IntegerField()
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField()
+    is_cumulative = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+
+class Seller(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name} - {self.country}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+    articul = models.CharField(max_length=20)
+    description = models.TextField()
+    count_on_stock = models.IntegerField()
+    discount = models.ForeignKey(Discount, on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -153,3 +153,17 @@ DJOSER = {
     'SEND_ACTIVATION_EMAIL': True,
     'ACTIVATION_URL': 'users/accounts/activate/{uid}/{token}',
 }
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'rest_framework_simplejwt.authentication.JWTAuthentication',
+)
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
