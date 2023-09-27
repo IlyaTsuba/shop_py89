014c367be39424a4028a308b0042c7253654e059
IlyaTsuba <ilyatsubablr@gmail.com>
2023-09-27 21:43:27 +0300

    add all guest functionality

--- shop/catalog/urls.py
+++ shop/catalog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from catalog.views import CategoryListView
-
+from catalog.views import CategoryListView, CategoryProductsView, DiscountsListView, DiscountProductsView, \
+    SellersListView, SellerProductsView
 
 urlpatterns = [
-    path('categories/', CategoryListView.as_view())
+    path('categories/', CategoryListView.as_view()),
+    path('categories/<int:category_id>/', CategoryProductsView.as_view()),
+    path('discounts/', DiscountsListView.as_view()),
+    path('discounts/<int:discount_id>/', DiscountProductsView.as_view()),
+    path('sellers/', SellersListView.as_view()),
+    path('sellers/<int:seller_id>/', SellerProductsView.as_view()),
 ]
