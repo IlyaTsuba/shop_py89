91822e7bdcf743ea3827f472c07f839fd3cae547
IlyaTsuba <ilyatsubablr@gmail.com>
2023-10-04 19:21:17 +0300

    basket functionality

--- shop/catalog/urls.py
+++ shop/catalog/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from catalog.views import CategoryListView, CategoryProductsView, DiscountsListView, DiscountProductsView, \
-    SellersListView, SellerProductsView
+    SellersListView, SellerProductsView, BasketView
 
 urlpatterns = [
     path('categories/', CategoryListView.as_view()),
     path('categories/<int:category_id>/', CategoryProductsView.as_view()),
+
     path('discounts/', DiscountsListView.as_view()),
     path('discounts/<int:discount_id>/', DiscountProductsView.as_view()),
+
     path('sellers/', SellersListView.as_view()),
     path('sellers/<int:seller_id>/', SellerProductsView.as_view()),
+
+    path('basket/', BasketView.as_view())
 ]
